30409e5152ad4e8c5b68cd593d4a68629f1747a1
sahasawat <sahasawat@techberry.co.th>
2023-11-02 09:32:54 +0700

    component collapse

---
+++ src/components/collapse/ZCollapse.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ZCollapse = () => {
+  return (
+    <>
+        <div className='p-1 bg-primary d-flex align-items-center rounded mb-2'>
+            <p className='text-light ms-1'>Navbar</p>
+        </div>
+        
+        <div className='p-1 bg-primary d-flex align-items-center rounded mb-2'>
+            <p className='text-light ms-1'>Form</p>
+        </div>
+
+        <div className='p-1 bg-primary d-flex align-items-center rounded mb-2'>
+            <p className='text-light ms-1'>Table</p>
+        </div>
+    </>
+  )
+}
+
+export default ZCollapse
--- src/pages/create-form-template/pages/form-create-template/FormCreateTemplate.jsx
+++ src/pages/create-form-template/pages/form-create-template/FormCreateTemplate.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Collapse} from "react-bootstrap"
+import ZCollapse from '../../../../components/collapse/ZCollapse'
 
 export const FormCreateTemplate = () => {
   return (
     <div className='p-3 rounded border'>
-        <button className='btn btn-primary'>Button</button>
+        {/* <button className='btn btn-primary'>Button</button> */}
+        <ZCollapse />
     </div>
   )
 }
